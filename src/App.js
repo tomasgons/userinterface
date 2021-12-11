f41ac15e45e494579132657371350c54cdfa83f4
tomasgons <tomasgons@gmail.com>
2021-12-11 17:52:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, } from 'react'
 
 
 import './App.css';
@@ -7,31 +7,20 @@ import UserForm from './components/UserForm';
 import UserList from './components/UserList';
 
 const firstUsers = [
-  {
-    id: 'e1',
-    name: 'Tomas',
-    age: 44
-  },
-  {
-    id: 'e2',
-    name: 'Jimi',
-    age: 77
-  },
-  {
-    id: 'e3',
-    name: 'Keith',
-    age: 123
-  }
+
 ]
 
 
 function App() {
   const [users, setUsers] = useState(firstUsers);
-
+  // useEffect(() => {
+  //   const firstUsers = localStorage.getItem('users');
+  //   setUsers(firstUsers ? JSON.parse(firstUsers) : []);
+  // }, [])
 
   const addUser = (user) => {
     setUsers(prevUsers => {
-      return [user, ...prevUsers]
+      return [user, ...prevUsers];
 
     })
   };
